6ba552c5c283571ff60effce6299d2c36e85bab4
byuantl <180382414+byuantl@users.noreply.github.com>
2025-09-29 15:15:41 -0700

    add: TAX

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -8,6 +9,7 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
         }
@@ -43,6 +45,22 @@ impl CPU {
                     return;
                 }
 
+                0xAA => {
+                    self.register_x = self.register_a;
+
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+
                 _ => todo!()
             }
         }
@@ -74,6 +92,31 @@ mod test {
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0xff, 0x00]);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }    
+
+   #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+       let mut cpu = CPU::new();
+       cpu.register_a = 10;
+       cpu.interpret(vec![0xaa, 0x00]);
+ 
+       assert_eq!(cpu.register_x, 10)
+    }
+
+    #[test]
+    fn test_0xaa_tax_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 0;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 0xff;
+        cpu.interpret(vec![0xaa, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
     }    
 }
