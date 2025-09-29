490b93c26c1ed4c93dead9eef63634eadf345da4
byuantl <180382414+byuantl@users.noreply.github.com>
2025-09-29 15:39:32 -0700

    add: INX

--- src/cpu.rs
+++ src/cpu.rs
@@ -25,6 +25,11 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_x);
     }
 
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if result == 0 {
             self.status = self.status | 0b0000_0010;
@@ -56,6 +61,8 @@ impl CPU {
                 }
 
                 0xAA => self.tax(),
+
+                0xE8 => self.inx(),
 
                 0x00 => return,
 
@@ -116,5 +123,22 @@ mod test {
         cpu.register_a = 0xff;
         cpu.interpret(vec![0xaa, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
-    }    
+    }
+    
+       #[test]
+   fn test_5_ops_working_together() {
+       let mut cpu = CPU::new();
+       cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+ 
+       assert_eq!(cpu.register_x, 0xc1)
+   }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 1)
+    }
 }
